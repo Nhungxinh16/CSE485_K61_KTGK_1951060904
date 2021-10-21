b69de8bac3565c386cda939efccdd402eea0fae0
Nhungxinh16 <tuyetnhung01062001@gmail.com>
2021-10-21 10:41:32 +0700

    sua index và them anh csdl

--- src/index.php
+++ src/index.php
@@ -3,7 +3,8 @@
     require("config/constants.php");
 ?>
     <div class="container">
-        <h2 class="text-center">Quản lý người hiến máu</h2>
+        <h2 class="text-center" style="margin-top: 40px; margin-bottom:24px;">Quản lý người hiến máu</h2>
+        <button type="button" class="btn btn-primary" style="margin-bottom: 12px"><a href="add.php" class="text-white">Thêm</a></button>
         <table class="table text-center">
             <thead class="bg-dark text-white">
             <tr>
@@ -13,6 +14,9 @@
                 <th>Số điện thoại</th>
                 <th>Lượng máu</th>
                 <th>Ngày đăng kí</th>
+                <th></th>
+                <th></th>
+                <th></th>
             </tr>
             </thead>
             <tbody>
@@ -34,13 +38,15 @@
                                 <td>".$row["reci_phno"]."</td>
                                 <td>".$row["reci_bqnty"]."mL</td>
                                 <td>".$row["reci_reg_date"]."</td>
+                                <td><a href='detail.php'>Xem chi tiết</a></td>
+                                <td><a href='update.php'>Sửa</a></td>
+                                <td><a href='delete.php'>Xóa</a></td>
                             </tr>
                         ";
                     }
                 ?>
             </tbody>
         </table>
-        <button type="button" class="btn btn-primary"><a href="add.php" class="text-white">Thêm</a></button>
     </div>
 
 
